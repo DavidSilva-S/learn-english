c08004b6d8c5efcb1458e687bffba5df5f649ec9
DavidSilva-S <davidemaildetrabalho@gmail.com>
2024-01-30 19:49:50 -0300

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+  import "../app.pcss";
+  import { Button } from "$lib/components/ui/button";
+  import { Input } from "$lib/components/ui/input"
+</script>
+
+<slot/>
+
+<div>
+  <h1 class="text-3xl font-bold underline">
+    Hey handsome man type your name below
+  </h1>
+  <Input type="email" placeholder="email" class="max-w-xs" />
+  <Button variant="destructive">Button</Button>
+</div>
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
